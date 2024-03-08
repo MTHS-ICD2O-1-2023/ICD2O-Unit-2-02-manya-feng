0a12d82becceed88c2ebe51833b092ebc40fa999
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-03-08 19:47:40 +0000

    change all the code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("perimeter-math").innerHTML = "<p>The perimeter is = " + (2 * (5 + 3)) + " cm </p>"
+  document.getElementById("area-math").innerHTML = "<p>The area is = " + (5 * 3) + " cm² </p>"
+
+}
